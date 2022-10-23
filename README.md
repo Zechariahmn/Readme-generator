4dd710a8d9c96f63e3f44b5808b5c1a327925428
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-10-22 20:26:36 -0500

    finished code

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# Readme-generator
-A node.js application capable of generating professional readme files
+
+  
+  # Readme-generator
+  ## Description
+  An aqpplication capable of generating professional readme files
+## Table Of Contents
+<li><a href="#installation">Installation</a></li>
+<li><a href="#usage">Usage</a></li>
+<li><a href="#license">License</a></li>
+<li><a href="#contributing">Contributing</a></li>
+<li><a href="#tests">Tests</a></li>
+<li><a href="#questions">Questions</a></li>
+
+## Installation
+To install necessary dependencies, run the following command:
+[npm install inquirer]
+## Usage
+In order for the user to use this repo, the user must first clone down the repo in order to have the necessary package.json, then use the [npm install inquirer] command in the command line to have the necessary inquirer module needed. The user can then use the [node index.js] command to start the application. Once the application is started it will ask the user a series of questions necessary to generate a readme file for the user.
+## License
+This project is licensed under the  
+## Contributing
+N/A
+## Tests
+[node index.js]
+## Questions
+If you have any feedback on what we can add to our application or any issues that you have run into, please feel free to reach out at my github https://github.com/zechariahmn or by email at zechariahnaitram20@gmail.com.
---
+++ index.js
@@ -0,0 +1,76 @@
+// TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is your project's name?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Please write a short description of your project",
+        name: "description"
+    },
+    {
+        type: "list",
+        message: "What kind of license should your project have?",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "What command should be run to install dependencies?",
+        name: "install"
+    },
+    {
+        type: "input",
+        message: "What command should be run to run tests?",
+        name: "tests"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about using the repo?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about contributing to the repo?",
+        name: "contributing"
+    }
+    
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    console.log(fileName);
+    console.log(data);
+    fs.writeFile(fileName, generateMarkdown(data), (err) => {
+        err ? console.log(err) : console.log(`Success! ${fileName} was generated`);
+    });
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((response) => {
+        writeToFile("README.md", response);
+    })    
+}
+
+// Function call to initialize app
+init();
